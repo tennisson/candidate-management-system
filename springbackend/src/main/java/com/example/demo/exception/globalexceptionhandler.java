0c64db00ca1f5d7d03669d166dd2e18d751b11ee
tennisson <148383335+tennisson@users.noreply.github.com>
2023-11-18 09:57:54 +0530

    Update globalexceptionhandler.java

--- springbackend/src/main/java/com/example/demo/exception/globalexceptionhandler.java
+++ springbackend/src/main/java/com/example/demo/exception/globalexceptionhandler.java
@@ -2,20 +2,28 @@ package com.example.demo.exception;
 
 import java.util.Date;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
+import com.example.demo.model.errorlogbean;
+import com.example.demo.repository.errorrepo;
+
 @ControllerAdvice
 public class globalexceptionhandler {
+	@Autowired
+	private errorrepo errepo;
 
 	// handling specific exception
 	@ExceptionHandler(WrongData.class)
 	public ResponseEntity<?> resourceNotFoundHandling(WrongData exception, WebRequest request){
 		errordetails errorDetails = 
 				new errordetails(new Date(), exception.getMessage(), request.getDescription(false));
+		errorlogbean eb=new errorlogbean( HttpStatus.NOT_FOUND ,exception.getMessage(),new Date());
+		errepo.save(eb);
 		return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
 	}
 
@@ -25,6 +33,8 @@ public class globalexceptionhandler {
 	public ResponseEntity<?> globalExceptionHandling(Exception exception, WebRequest request){
 		errordetails errorDetails = 
 				new errordetails(new Date(), exception.getMessage(), request.getDescription(false));
+		errorlogbean eb=new errorlogbean( HttpStatus.NOT_FOUND ,exception.getMessage(),new Date());
+		errepo.save(eb);
 		return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 }
